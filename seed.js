4222f040363b74b8d0df938e04eacb0db16e77d3
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-09-29 10:24:08 -0700

    create seed data and html boilerplate

---
+++ seed.js
@@ -0,0 +1,73 @@
+var pets = [];
+  pets.push({
+    name: "Fabio",
+    type: "dog",
+    age: "14 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "male",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Beethoven",
+    type: "dog",
+    age: "5 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "male",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Garfield",
+    type: "cat",
+    age: "38 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "male",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Tookie",
+    type: "bird",
+    age: "19 years old",
+    vaccination: "yes",
+    neutered: "no",
+    gender: "male",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Love",
+    type: "cat",
+    age: "4 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "female",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Olive",
+    type: "chinchilla",
+    age: "3 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "female",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Poochers",
+    type: "dog",
+    age: "4 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "female",
+    image: "http://image.com"
+  });
+  pets.push({
+    name: "Stuart",
+    type: "mouse",
+    age: "17 years old",
+    vaccination: "yes",
+    neutered: "yes",
+    gender: "male",
+    image: "http://image.com"
+  });
