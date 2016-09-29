ed824a659dc781926a81a5df3525cf8a70ae469b
Natalia Hess <nathess91@gmail.com>
2016-09-29 10:50:23 -0700

    change values for seed.js

--- seed.js
+++ seed.js
@@ -1,73 +1,73 @@
-var pets = [];
-  pets.push({
-    name: "Fabio",
-    type: "dog",
-    age: "14 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "male",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Beethoven",
-    type: "dog",
-    age: "5 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "male",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Garfield",
-    type: "cat",
-    age: "38 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "male",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Tookie",
-    type: "bird",
-    age: "19 years old",
-    vaccination: "yes",
-    neutered: "no",
-    gender: "male",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Love",
-    type: "cat",
-    age: "4 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "female",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Olive",
-    type: "chinchilla",
-    age: "3 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "female",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Poochers",
-    type: "dog",
-    age: "4 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "female",
-    image: "http://image.com"
-  });
-  pets.push({
-    name: "Stuart",
-    type: "mouse",
-    age: "17 years old",
-    vaccination: "yes",
-    neutered: "yes",
-    gender: "male",
-    image: "http://image.com"
-  });
+var petList = [];
+petList.push({
+  name: "Fabio",
+  type: "dog",
+  age: "14 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "male",
+  image: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13150791_645581085597263_1211879218_n.jpg?ig_cache_key=MTI0NjYzODY4MjcwODMwNzExNQ%3D%3D.2"
+});
+petList.push({
+  name: "Beethoven",
+  type: "dog",
+  age: "5 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "male",
+  image: "http://www.coupay.com/topoften/wp-content/uploads/2014/01/images-33.jpg"
+});
+petList.push({
+  name: "Garfield",
+  type: "cat",
+  age: "38 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "male",
+  image: "https://www.cs.cmu.edu/afs/cs.cmu.edu/academic/class/15463-f13/www/proj2/www/bcharas/worst/garfield/garfield_cat.jpg"
+});
+petList.push({
+  name: "Tookie",
+  type: "bird",
+  age: "19 years old",
+  vaccination: true,
+  fixed: false,
+  gender: "male",
+  image: "http://www.animalspot.net/wp-content/uploads/2016/02/Toco-Toucan-Pictures.jpg"
+});
+petList.push({
+  name: "Love",
+  type: "cat",
+  age: "4 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "female",
+  image: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13473237_1758580281032310_436342857_n.jpg?ig_cache_key=MTI4Mzg0MTQxMDMyNzU2MDEzMw%3D%3D.2"
+});
+petList.push({
+  name: "Olive",
+  type: "chinchilla",
+  age: "3 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "female",
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/02/Chinchilla-Soelvmarmorert.jpg"
+});
+petList.push({
+  name: "Poochers",
+  type: "dog",
+  age: "4 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "female",
+  image: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13707160_272207893151806_346874866_n.jpg?ig_cache_key=MTMxMDc3OTY3MzY4OTM1ODU1MA%3D%3D.2"
+});
+petList.push({
+  name: "Stuart",
+  type: "mouse",
+  age: "17 years old",
+  vaccination: true,
+  fixed: true,
+  gender: "male",
+  image: "https://media.salon.com/2002/07/stuart_little_2.jpg"
+});
