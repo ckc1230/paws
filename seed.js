f998c99e49a2d1a2a37a6682102593b532c8d188
Natalia Hess <nathess91@gmail.com>
2016-10-03 12:17:48 -0700

    Update seed.js

--- seed.js
+++ seed.js
@@ -7,7 +7,7 @@ petList.push({
 	name: "Theo",
 	type: "dog",
 	age: "1",
-	likes: "13",
+	interested: "13",
 	vaccination: true,
 	fixed: true,
 	gender: "male",
@@ -19,299 +19,299 @@ petList.push({
 	name: "Other Dog",
 	type: "dog",
 	age: "4",
-	likes: "13",
+	interested: "13",
 	vacination: true,
 	fixed: true,
 	gender: "female",
 	picture: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/14280350_175479136192510_3532240967484571648_n.jpg?ig_cache_key=MTM0NjAwODgxNzc3NDk2MTcyMw%3D%3D.2",
-	owner: "greg"
+	owner: "ilias"
 })
 
 petList.push({
 	name: "Maggie",
 	type: "cat",
 	age: "2",
-	likes: "13",
+	interested: "13",
 	vacination: true,
 	fixed: true,
 	gender: "female",
 	picture: 'https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-0/p206x206/11949497_10153072748752727_3155146891262570335_n.jpg?oh=764c714d8d3d33d10ea070c1ea3a9466&oe=5875EFAF',
-	owner: "noah"
+	owner: "ilias"
 })
 
 petList.push({
 	name: "Louisa",
 	type: "bird",
 	age: "15",
-	likes: "13",
+	interested: "13",
 	vaccination: true,
 	fixed: true,
 	gender: "female",
 	picture: 'https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-0/p206x206/1238209_10201027775790085_100547095_n.jpg?oh=40f8930ac8693e44697d0b478a0a4a7c&oe=5868CE72',
-	owner: "jeff"
+	owner: "ilias"
 })
 
 petList.push({
 	name: "Gus",
 	type: "cat",
 	age: "12",
-	likes: "13",
+	interested: "13",
 	vaccination: true,
 	fixed: true,
 	gender: "male",
 	picture: '',
-	owner: "shirley"
+	owner: "ilias"
 })
 
 petList.push({
 	name: "Stella",
 	type: "cat",
 	age: "15",
-	likes: "13",
+	interested: "13",
 	vaccination: true,
 	fixed: true,
 	gender: "female",
 	picture: '',
-	owner: "pickapet"
+	owner: "ilias"
 })
 
 petList.push({
   name: "Snuffles",
   type: "cat",
   age: "3",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "http://petful.supercopyeditors.netdna-cdn.com/wp-content/uploads/2016/02/14032864382_0566a75778_c-750x501.jpg",
-  owner: "pickapet"
+  owner: "ilias"
 })
 
 petList.push({
   name: "Peanut",
   type: "hamster",
   age: "1",
-	likes: "13",
+	interested: "13",
   vaccination: false,
   fixed: false,
   gender: "male",
   picture: "http://www.mrjohnsons.co.uk/uploads/small/copy_hamster_nutc.jpg",
-  owner: "ilias"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "DJ Shadez, MD",
   type: "dog",
   age: "6",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "female",
   picture: "http://1.bp.blogspot.com/-4LEQPEDj4IQ/UD6pwk3dwLI/AAAAAAAAAjI/mzmMLCC5P5U/s640/black-poodle-ch%2Bjaset's%2Bsatisfaction.jpg",
-  owner: "jeff"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Big Bow Wow",
   type: "dog",
   age: "5",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11358034_1458243211142525_1167013190_n.jpg",
-  owner: "mitch"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Ms. Sassy",
   type: "turtle",
   age: "3",
-	likes: "13",
+	interested: "13",
   vaccination: false,
   fixed: false,
   gender: "female",
   picture: "http://www.thepetmatchmaker.com/wp-content/uploads/2014/08/fd428d2622b607196dab24e277422cbf-1.jpg",
-  owner: "martha"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Bob",
   type: "cat",
   age: "9",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "https://media.giphy.com/media/pVkmGyqYRt4qY/giphy-facebook_s.jpg",
-  owner: "noah"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Cupcake",
   type: "dog",
   age: "1",
-	likes: "13",
+	interested: "13",
   vaccination: false,
   fixed: false,
   gender: "female",
   picture: "http://www.lazerhorse.org/wp-content/uploads/2014/06/Pomeranian-Puppy-sleeping.jpg",
-  owner: "ilias"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Handsome",
   type: "ant",
   age: "1",
-	likes: "13",
+	interested: "13",
   vaccination: false,
   fixed: false,
   gender: "male",
   picture: "http://www.indianapest.com/images/Ant_-_10.jpg",
-  owner: "mitch"
+  owner: "Ilias"
 })
 
 petList.push({
   name: "Fabio",
   type: "dog",
   age: "14 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13150791_645581085597263_1211879218_n.jpg?ig_cache_key=MTI0NjYzODY4MjcwODMwNzExNQ%3D%3D.2",
-  owner: "ilias"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Beethoven",
   type: "dog",
   age: "5 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "http://www.coupay.com/topoften/wp-content/uploads/2014/01/images-33.jpg",
-  owner: "noah"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Garfield",
   type: "cat",
   age: "38 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "male",
   picture: "https://www.cs.cmu.edu/afs/cs.cmu.edu/academic/class/15463-f13/www/proj2/www/bcharas/worst/garfield/garfield_cat.jpg",
-  owner: "jeff"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Tookie",
   type: "bird",
   age: "19 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: false,
   gender: "male",
   picture: "http://www.animalspot.net/wp-content/uploads/2016/02/Toco-Toucan-Pictures.jpg",
-  owner: "ilias"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Love",
   type: "cat",
   age: "4 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "female",
   picture: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13473237_1758580281032310_436342857_n.jpg?ig_cache_key=MTI4Mzg0MTQxMDMyNzU2MDEzMw%3D%3D.2",
-  owner: "martha"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Olive",
   type: "chinchilla",
   age: "3 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "female",
   picture: "https://upload.wikimedia.org/wikipedia/commons/0/02/Chinchilla-Soelvmarmorert.jpg",
-  owner: "ilias"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Poochers",
   type: "dog",
   age: "4 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: "female",
   picture: "https://scontent-yyz1-1.cdninstagram.com/t51.2885-15/e35/13707160_272207893151806_346874866_n.jpg?ig_cache_key=MTMxMDc3OTY3MzY4OTM1ODU1MA%3D%3D.2",
-  owner: "greg"
+  owner: "Ilias"
 });
 
 petList.push({
   name: "Stuart",
   type: "mouse",
   age: "17 years old",
-	likes: "13",
+	interested: "13",
   vaccination: true,
   fixed: true,
   gender: 'male',
   picture: 'https://media.salon.com/2002/07/stuart_little_2.jpg',
-  owner: "mitch"
+  owner: "ilias"
 });
 
 var ownerList = [];
 
 ownerList.push({
 	name: 'ilias',
-	email: 'Ilias@ga.co',
+	email: 'Ilias@generalassemb.ly',
 	location: '15 Lomardi Street',
 });
 
 ownerList.push({
-	name: 'jeff',
+	name: 'Jeff',
 	email: 'Mydad@hisemail.com',
 	location: 'New Rochelle, NY',
 });
 ownerList.push({
-	name: 'noah',
+	name: 'Noah',
 	email: 'mybrother@hisemail.com',
 	location: 'Brooklyn, NY'
 });
 
 ownerList.push({
- 	name: "shirley",
+ 	name: "Shirley",
     email: "girleyshirley2hawts@gmail.com",
     location: "San Leandro, CA"
 });
 
 ownerList.push({
-    name: "mitch",
+    name: "Mitch",
     email: "iammitch@gmail.com",
     location: "Berkeley, CA"
 });
 
 ownerList.push({
-    name: "martha",
+    name: "Martha",
     email: "bakingislife@gmail.com",
     location: "Palo Alto, CA"
 });
 
 ownerList.push({
-	name: "greg",
+	name: "Greg",
     email: "ilovecat5@gmail.com",
     location: "San Francisco, CA"
 });
 
 ownerList.push({
-	name: 'pickapet',
+	name: 'Pick A Pet',
 	email: 'getapet@pickapet.com',
 	location: '255 Busch Street'
 });
@@ -324,15 +324,7 @@ ownerList.push({
 
 db.Pet.remove({}, function(err, pets){
   petList.forEach(function (petData) {
-    var pet = new db.Pet({
-      name: petData.name,
-      type: petData.type,
-      age: petData.age,
-      vaccination: petData.vaccination,
-      fixed: petData.fixed,
-      gender: petData.gender,
-      picture: petData.picture
-    });
+    var pet = new db.Pet(petData)
     db.Owner.findOne({name: petData.owner}, function (err, foundOwner) {
       if (err) {
         console.log('we fucked up');
