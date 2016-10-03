85eeba388df76b8342cafa9abbac8f948f6555dd
Natalia Hess <nathess91@gmail.com>
2016-10-03 12:25:45 -0700

    Update pets.js

--- models/pets.js
+++ models/pets.js
@@ -7,7 +7,10 @@ var PetSchema = new Schema({
 	name: String,
 	type: String,
 	age: String,
-	interesed: String,
+	interested: {
+		type: Number,
+		default: 0
+	},
 	vaccination: Boolean,
 	fixed: Boolean,
 	gender: String,
